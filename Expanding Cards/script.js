c4f1ad9a266b9e877b167fb44a33a01e4f2e94ff
attano001 <attano001@outlook.com>
2022-04-02 12:01:55 +0100

    First Commit.

---
+++ Expanding Cards/script.js
@@ -0,0 +1,15 @@
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    if (panel.classList.contains("active")) return;
+    else removeActive();
+    panel.classList.add("active");
+  });
+});
+
+function removeActive() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
